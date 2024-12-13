75ac5f42f3e4e735516d762e515fa6fc8c964b44
shemaroger <shemaroger60@gmail.com>
2024-12-13 22:32:47 +0200

    update

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -35,7 +35,7 @@ function Login() {
     setIsLoading(true);
 
     try {
-      const response = await fetch('http://localhost:8080/api/login', {
+      const response = await fetch('http://localhost:8080/api/users/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
